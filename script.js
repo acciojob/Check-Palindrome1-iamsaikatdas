8914ae1decfcf4d4f17588e43b0008f55f6c4312
Saikat Das <102199984+iamsaikatdas@users.noreply.github.com>
2023-04-13 07:51:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-
+	const cleanedStr = str.replace(/[\W_]/g, '').toLowerCase();
+  // reverse the cleaned string
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  // compare the cleaned string with its reversed version
+  return cleanedStr === reversedStr;
 }
 module.exports = palindrome
